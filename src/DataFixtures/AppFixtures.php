0cf8f4c3d0e61c6696690b92b7accc004e39821a
Francis <dinfrancis@hotmail.fr>
2020-05-14 16:10:59 +0200

    Creation user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,20 +3,58 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
-use App\Entity\Image;
 use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class AppFixtures extends Fixture 
-{
+{   
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    
     public function load(ObjectManager $manager)
     {   
         
         $faker = Factory::create("FR-fr");
 
+        // Nous gérons les utilisateurs
 
+        $user = [];
+        $genres = ['male', 'female'];
+
+        for($i = 1;$i <= 30;$i++){
+            $user = new User();
+
+            $genre = $faker->randomElement($genres);
+
+            $picture = 'https://randomuser.me/api/portraits/';
+            $pictureId = $faker->numberBetween(1,99) . '.jpg';
+
+            $picture = $picture . ($genre == 'male' ? 'men/' : 'women/') . $pictureId;
+
+            $hash = $this->encoder->encodePassword($user, 'password');
+ 
+            $user->setlastName($faker->lastName($genre))
+                 ->setfirstName($faker->firstName)
+                 ->setEmail($faker->email)
+                 ->setIntroduction($faker->sentence())
+                 ->setDescription('<p>'. join('</p><p>', $faker->paragraphs(3)) .'</p>')
+                 ->setHash($hash)
+                 ->setPicture($picture);
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Nous gérons les annonces 
         for($i = 1;$i <= 30; $i++){
 
             // On veux crée une nouvelle annonce
@@ -28,12 +66,15 @@ class AppFixtures extends Fixture
             $introduction = $faker->paragraph(2);
             $content      = '<p>'. join('</p><p>', $faker->paragraphs(5)) .'</p>';
 
+            $user = $users[mt_rand(0, count($users) -1)];
+
             $ad ->setTitle($title)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(30, 80)) // La propriété mt_rand offre la positibilié tde cibler deux nombres 
-                ->setRooms(mt_rand(1,5)); // La propriété mt_rand offre la positibilié tde cibler deux nombres
+                ->setRooms(mt_rand(1,5)) // La propriété mt_rand offre la positibilié tde cibler deux nombres
+                ->setAuthor($user);
             
             // Les annonces auront entre 2 et 5 images d'ou la boucle For
             for($j = 1;$j <= mt_rand(2,5); $j++){
--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -80,6 +80,12 @@ class Ad
      */
     private $images;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="yes")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
     public function __construct()
     {
         $this->images = new ArrayCollection();
@@ -218,6 +224,18 @@ class Ad
                 $image->setAd(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
