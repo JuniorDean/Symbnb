d4a6fa305872bb337eba50afaf5dbb6e8cf02a7e
Francis <dinfrancis@hotmail.fr>
2020-05-28 11:52:10 +0200

    gestion des utilisateurs, liaison des annonces

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,17 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity(
+ * 
+ * fields={"email"},
+ * message="Un autre utilisateur s'est déjà inscrit avec cette adresse email, Veuillez la modifier"
+ * )
  */
 class User implements UserInterface
 {
@@ -23,16 +30,19 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Vous devez renseigner votre prénom") // Non Vide
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Vous devez renseigner votre nom") // Non Vide
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+    * @Assert\Email(message="Veuillez renseigner un email valide")
      */
     private $email;
 
@@ -43,16 +53,26 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * Assert\Url(message="Veuillez donner une URL pour votre avatar")
      */
     private $hash;
 
+    
+    /**
+     * @Assert\EqualTo(propertyPath="hash", message="Vous n'avez pas correctement confirmé votre mot de passe !")
+     * Ici on souhaite que le passwordConfirm soit strictement égal a hash  
+     */
+    public $passwordConfirm;
+
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="10", minMessage="Votre introduction doit faire au moins 10 caractères")
      */
     private $introduction;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(min="100",minMessage="Votre description doit faire au moins 100 caractères")
      */
     private $description;
 
@@ -60,6 +80,19 @@ class User implements UserInterface
      * @ORM\Column(type="string", length=255)
      */
     private $slug;
+
+    
+    /**
+
+     * @ORM\OneToMany(targetEntity="App\Entity\Ad", mappedBy="author")
+
+     */
+
+    private $ads;
+
+    public function getFullName(){
+        return "{$this->firstName} {$this->lastName}";
+    }
     
     /**
      * Permet d'initialiser le slug
@@ -177,6 +210,20 @@ class User implements UserInterface
         $this->slug = $slug;
 
         return $this;
+    }
+
+    /**
+
+     * @return Collection|Ad[]
+
+     */
+
+    public function getAds(): Collection
+
+    {
+
+        return $this->ads;
+
     }
 
     public function getRoles(){
